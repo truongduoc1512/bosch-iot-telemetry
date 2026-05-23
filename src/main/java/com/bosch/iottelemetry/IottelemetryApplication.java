8a7848b8d14193a258bcb6352653f22b5ba13258
truongduoc1512 <hoaiduoc15@gmail.com>
2026-05-23 10:59:12 +0700

    chore: initialize Spring Boot project with MySQL docker compose

---
+++ src/main/java/com/bosch/iottelemetry/IottelemetryApplication.java
@@ -0,0 +1,13 @@
+package com.bosch.iottelemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IottelemetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IottelemetryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bosch/iottelemetry/IottelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosch.iottelemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IottelemetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
